886f1bf52d975361aa503682c675591b1181337d
Zayd <zaydbenfathel@gmail.com>
2025-04-02 20:39:30 +0100

    completed lecture 5

--- Web Programming with Python and JavaScript/Lectures/Week 6/Lecture 5/counter.js
+++ Web Programming with Python and JavaScript/Lectures/Week 6/Lecture 5/counter.js
@@ -2,7 +2,7 @@ if (!localStorage.getItem('counter')){
     localStorage.setItem('counter', 0);
 }
 function count(){
-    let counter = localStorage.getItem('counter')
+    let counter = localStorage.getItem('counter') 
     counter++;
     document.querySelector("h1").innerHTML = counter;
     localStorage.setItem('counter', counter);
